90d2ff590082d13387e8f311ec30bf30597c06a5
Quod <36698256+Quod6@users.noreply.github.com>
2020-02-23 22:28:26 +1000

    Add files via upload

--- Lidar.py
+++ Lidar.py
@@ -1,20 +1,17 @@
 import rplidar as rp
 import time
-lidar = rp.RPLidar("COM3", 115200)
-print(lidar.get_health())
-li = lidar.iter_measurments(50000)
-# print(read)
 
 
-class PromobotLidar:
+class PromobotLidar():
 
     def __init__(self, PORT):
-        self.lidar = rp.RPLidar(PORT, 115200)
+        self = rp.RPLidar(PORT, 115200)
 
     def searchforward(self, interval):
-        # interval - the range of data read values, the "field of view" of the lidar
+        """Interval - the range of data read values, the "field of view" of the lidar"""
         distance = 0
-        for scan in lidar.iter_scans():
+        read = self.iter_scans()
+        for scan in self.iter_scans():
             try:
                 for i in range(len(scan)):
                     if scan[i][1] >= 360-(interval/2) and scan[i][1] < interval/2:
@@ -25,6 +22,9 @@ class PromobotLidar:
         return distance
 
 
-lidar.clear_input()
-time.sleep(1)
 
+
+if __name__ == '__main__':
+    lidar1 = PromobotLidar("COM3")
+    r = lidar1.searchforward(2)
+    print(r)
