9ca3e7ab86e3eebced09ff0e0c525c994e36b2ff
Quod <36698256+Quod6@users.noreply.github.com>
2020-02-24 16:24:39 +1000

    Add files via upload

--- Lidar.py
+++ Lidar.py
@@ -1,5 +1,6 @@
 import rplidar as rp
-import cv2
+from PIL import Image as img
+import numpy as np
 import time
 import threading as tr
 
@@ -9,21 +10,25 @@ class PromobotLidar():
 
     def __init__(self, PORT):
         self.lidar = rp.RPLidar(PORT, 115200)
-        '''while 1:
+        '''
+        while 1:
             try:
                 print(PORT)
-                #self.lidar = rp.RPLidar(PORT, 115200)
-            except rp.RPLidarException as e:
+                self.lidar = rp.RPLidar(PORT, 115200)
+            except Exception as e:
                 print(e)
                 print("Подключите Лидар или укажите другой порт")
-                PORT1 = upper(input("Укажите порт: "))
+                PORT1 = input("Укажите порт: ").upper()
                 if PORT1 == "":
                     continue
                 else:
                     PORT = PORT1
                     print(PORT)
                     continue
+            else:
+                break
         '''
+
 
 
     def searchforward(self, interval):
@@ -40,11 +45,14 @@ class PromobotLidar():
                 print(E)
         return distance
 
-    def startdrawmap(self, weight, hight):
+    def startdrawmap(self, width, height):
         if self.flagdraw == False:
             self.flagdraw = True
+        map = img.new('RGB', (width, height), (0, 0, 120))
+        centerX = int(width/2)
+        centerY = int(height/2)
         while self.flagdraw:
-            pass
+            map.show()
 
     def stopdrawmap(self):
         self.flagdraw = False
@@ -52,5 +60,5 @@ class PromobotLidar():
 
 if __name__ == '__main__':
     lidar1 = PromobotLidar("COM3")
-    r = lidar1.searchforward(90)
+    r = lidar1.startdrawmap(800, 600)
     print(r)
